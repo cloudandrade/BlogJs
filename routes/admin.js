2bcf05b5fe010e04d60ee5be92021a00b3a3ff0b
Jan Cloud <cloud.andrade16@gmail.com>
2019-07-02 12:35:26 -0300

    cadastro de categorias
    armazenando categorias no banco de dados

--- routes/admin.js
+++ routes/admin.js
@@ -1,5 +1,8 @@
 const express = require('express')
 const router = express.Router();
+const mongoose = require("mongoose")
+require("../models/Categoria")//importando o model para ser usado
+const Categoria = mongoose.model("categorias") //passando o valor do model para uma variavel
 
 router.get('/', (req, res) =>{
     res.render("admin/index")
@@ -17,9 +20,25 @@ router.get('/categorias/add', (req, res) => {
     res.render("admin/addcategorias")
 })
 
+router.post('/categorias/nova', (req, res) => {
+    const novaCategoria = { //requisitando os dados da página colocado nos inputs e criando um objeto
+        nome: req.body.nome,
+        slug: req.body.slug
+    }
+
+    new Categoria(novaCategoria).save().then(() => {
+        console.log("categoria salva com sucesso!")
+    }).catch((erro) => {
+        console.log("não foi possível salvar a categoria" + erro)
+    })
+
+ })
+
 router.get('/teste', (req, res) => {
     res.send("Página teste")
 })
+
+
 
 //exportando router -  o router será responsavel por administrar nossas rotas
 module.exports = router
